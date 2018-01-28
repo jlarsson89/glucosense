3c745c37167e4f932525df95bdec5b3b444eeb6b
Johan Larsson <johan.larsson1989@gmail.com>
2018-01-28 21:17:52 +0000

    added activities

---
+++ app/src/main/java/glucosense/org/MealActivity.kt
@@ -0,0 +1,12 @@
+package glucosense.org
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MealActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal)
+    }
+}
