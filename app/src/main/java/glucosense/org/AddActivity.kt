410cd6e5075197dd9e5ea7233045c2196b847ece
Johan Larsson <johan.larsson1989@gmail.com>
2018-02-04 02:14:21 +0000

    addactivity now has layout and functioning back button

--- app/src/main/java/glucosense/org/AddActivity.kt
+++ app/src/main/java/glucosense/org/AddActivity.kt
@@ -1,12 +1,20 @@
 package glucosense.org
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_add.*
 
 class AddActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+        backButton.setOnClickListener {
+            Log.i("press", "back")
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
