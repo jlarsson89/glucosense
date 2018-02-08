6649110bbfab508b8899a7509a8dc5b1e4fa9f0f
Johan Larsson <johan.larsson1989@gmail.com>
2018-02-08 21:00:30 +0000

    date picker dialog works, need to fix time and correctly storing

--- app/src/main/java/glucosense/org/InjectionActivity.kt
+++ app/src/main/java/glucosense/org/InjectionActivity.kt
@@ -1,12 +1,33 @@
 package glucosense.org
 
+import android.app.DatePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class InjectionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_injection)
+        val editText = findViewById<View>(R.id.editText2)
+        val cal = Calendar.getInstance()
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, day ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, day)
+            val format = "yy.MM.dd"
+            val sdf = SimpleDateFormat(format, Locale.UK)
+            //editText.text = sdf.format(cal.time)
+        }
+        editText.setOnClickListener {
+            DatePickerDialog(this@InjectionActivity, dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
     }
 }
