aa25fae2ec74c36a0e109b537789475ea6b7a19a
Johan Larsson <johan.larsson1989@gmail.com>
2018-02-09 22:18:17 +0000

    added timepicker

--- app/src/main/java/glucosense/org/InjectionActivity.kt
+++ app/src/main/java/glucosense/org/InjectionActivity.kt
@@ -1,6 +1,8 @@
 package glucosense.org
 
 import android.app.DatePickerDialog
+import android.app.ProgressDialog.show
+import android.app.TimePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,21 +15,32 @@ class InjectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_injection)
-        val editText = findViewById<View>(R.id.editText2)
+        val editDate = findViewById<View>(R.id.datePicker)
+        val editTime = findViewById<View>(R.id.timePicker)
         val cal = Calendar.getInstance()
         val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, day ->
             cal.set(Calendar.YEAR, year)
             cal.set(Calendar.MONTH, month)
             cal.set(Calendar.DAY_OF_MONTH, day)
-            val format = "yy.MM.dd"
-            val sdf = SimpleDateFormat(format, Locale.UK)
+            //editDate.text = SimpleDateFormat("yyyy-MM-dd").format(cal.time)
             //editText.text = sdf.format(cal.time)
         }
-        editText.setOnClickListener {
+        editDate.setOnClickListener {
             DatePickerDialog(this@InjectionActivity, dateSetListener,
                     cal.get(Calendar.YEAR),
                     cal.get(Calendar.MONTH),
                     cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+            val format = "HH:MM"
+        }
+        editTime.setOnClickListener {
+            TimePickerDialog(this@InjectionActivity, timeSetListener,
+                    cal.get(Calendar.HOUR_OF_DAY),
+                    cal.get(Calendar.MINUTE),
+                    true).show()
         }
     }
 }
