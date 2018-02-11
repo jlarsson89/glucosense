45ae98ee2b4dbc6db1dbc8dc4aff21006748ab0e
Johan Larsson <johan.larsson1989@gmail.com>
2018-02-11 23:09:21 +0000

    cleaned up settings

--- app/src/main/java/glucosense/org/HistoryActivity.kt
+++ app/src/main/java/glucosense/org/HistoryActivity.kt
@@ -1,13 +1,28 @@
 package glucosense.org
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import com.beust.klaxon.json
+import kotlinx.android.synthetic.main.activity_history.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 class HistoryActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
-
+        //val array = parse.parse("/e.json") as JsonArray<JsonObject>
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
