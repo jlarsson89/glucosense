1f43e94ec44a56af6f47448e818c42135e315287
Johan Larsson <johan.larsson1989@gmail.com>
2018-02-18 02:51:14 +0000

    added preliminary nfc functionality

--- app/src/main/java/glucosense/org/ScanActivity.kt
+++ app/src/main/java/glucosense/org/ScanActivity.kt
@@ -2,6 +2,9 @@ package glucosense.org
 
 import android.content.Context
 import android.content.Intent
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.NfcAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -25,6 +28,40 @@ class ScanActivity : AppCompatActivity() {
             Log.d("test", "works")
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)        }
+    }
+
+    private fun getNDefMessages(intent: Intent): Array<NdefMessage>  {
+        val rawMessage = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+        rawMessage?.let {
+            return rawMessage.map {
+                it as NdefMessage
+            }.toTypedArray()
+        }
+        val empty = byteArrayOf()
+        val record = NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty)
+        val msg = NdefMessage(arrayOf(record))
+        return arrayOf(msg)
+    }
+
+    fun retrieveNFCMessage(intent: Intent?): String {
+        intent?.let {
+            if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+                val ndefMessages = getNDefMessages(intent)
+                ndefMessages[0].records?.let {
+                    it.forEach {
+                        it?.payload.let {
+                            it?.let {
+                                return String(it)
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                return "Touch NFC tag to read data"
+            }
+        }
+        return "Touch NFC tag to read data"
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
