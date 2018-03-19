c6062f817b4b562c55df3ae9708b7fe88e5b135b
Johan Larsson <johan.larsson1989@gmail.com>
2018-03-19 00:21:08 +0000

    added addmeal and delmeal functions, minor tweaks to model

--- app/src/main/java/glucosense/org/Ingredient.kt
+++ app/src/main/java/glucosense/org/Ingredient.kt
@@ -1,15 +1,19 @@
 package glucosense.org
 
 import io.realm.RealmObject
+import io.realm.RealmResults
+import io.realm.annotations.LinkingObjects
 import io.realm.annotations.PrimaryKey
 
 open class Ingredient(
         @PrimaryKey open var _name: String = "",
+        open var parent: String = "",
         open var quantity: String = ""
 )
     : RealmObject() {
     fun copy(
             _name: String = this._name,
+            parent: String = this.parent,
             quantity: String = this.quantity
-    ) = Injection(_name, quantity)
+    ) = Ingredient(_name, parent, quantity)
 }
--- app/src/main/java/glucosense/org/MealModel.kt
+++ app/src/main/java/glucosense/org/MealModel.kt
@@ -1,16 +1,33 @@
 package glucosense.org
 
+import android.util.Log
 import io.realm.Realm
 import io.realm.RealmResults
 
 class MealModel : MealInterface {
     override fun addMeal(realm: Realm, meal: Meal): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        try {
+            realm.beginTransaction()
+            realm.copyToRealmOrUpdate(meal)
+            realm.commitTransaction()
+            return true
+        }
+        catch (e: Exception) {
+            Log.i("exception", e.toString())
+            return false
+        }    }
 
     override fun delMeal(realm: Realm, _ID: String): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        try {
+            realm.beginTransaction()
+            realm.where(Meal::class.java).equalTo("_ID", _ID).findFirst()?.deleteFromRealm()
+            realm.commitTransaction()
+            return true
+        }
+        catch (e: Exception) {
+            Log.i("exception", e.toString())
+            return false
+        }    }
 
     override fun editMeal(realm: Realm, meal: Meal): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
