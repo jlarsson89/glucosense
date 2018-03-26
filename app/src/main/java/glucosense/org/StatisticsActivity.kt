a665f62f424f007067758d6430ad5070e2462521
Johan Larsson <johan.larsson1989@gmail.com>
2018-03-26 01:28:57 +0100

    added statistics stuff

--- app/src/main/java/glucosense/org/StatisticsActivity.kt
+++ app/src/main/java/glucosense/org/StatisticsActivity.kt
@@ -2,8 +2,10 @@ package glucosense.org
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_statistics.*
+import java.util.*
 
 class StatisticsActivity : AppCompatActivity() {
     private var injectionModel = InjectionModel()
@@ -14,9 +16,15 @@ class StatisticsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
+        val cal = Calendar.getInstance()
+        cal.set(2018, 0, 0) // shows incorrect date but correct format
+        val currentTime = cal.get(Calendar.YEAR).toString() + "-" + cal.get(Calendar.MONTH + 1) + "-" + cal.get((Calendar.DAY_OF_MONTH))
+        Log.i("time", (cal.get(Calendar.YEAR).toString()) + "-" + cal.get(Calendar.MONTH + 1).toString() + "-" + cal.get(Calendar.DAY_OF_MONTH))
+        Log.i("currenttime", currentTime)
         injectionModel = InjectionModel()
         mealModel = MealModel()
         ingredientModel = IngredientModel()
         injTotalText.text = injectionModel.getInjections(realm).size.toString()
+        lastMealTimeText.text = mealModel.getMeals(realm).last()?._ID
     }
 }
