52ec23cf8f6cf631139a73d0ef50677f9469c2c1
Johan Larsson <johan.larsson1989@gmail.com>
2018-03-30 21:52:13 +0100

    added api call skeleton

--- app/src/main/java/glucosense/org/StatisticsActivity.kt
+++ app/src/main/java/glucosense/org/StatisticsActivity.kt
@@ -5,18 +5,24 @@ import android.os.Bundle
 import android.util.Log
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_statistics.*
+import java.net.URL
 import java.util.*
+import java.util.concurrent.Executors
 
 class StatisticsActivity : AppCompatActivity() {
     private var injectionModel = InjectionModel()
     private var mealModel = MealModel()
     private var ingredientModel = IngredientModel()
     var realm = Realm.getDefaultInstance()
+    val url = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
         val cal = Calendar.getInstance().time
+        val data = Executors.newSingleThreadExecutor().execute({
+            val json = URL(url).readText()
+        })
         //cal.set(2018, 0, 0) // shows incorrect date but correct format
         //val currentTime = cal.get(Calendar.YEAR).toString() + "-" + cal.get(Calendar.MONTH + 1) + "-" + cal.get((Calendar.DAY_OF_MONTH))
         //Log.i("time", (cal.get(Calendar.YEAR).toString()) + "-" + cal.get(Calendar.MONTH + 1).toString() + "-" + cal.get(Calendar.DAY_OF_MONTH))
