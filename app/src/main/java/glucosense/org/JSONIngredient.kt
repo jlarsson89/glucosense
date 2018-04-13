aaba97e8f82ec6c1783f9093a7ab7e8916e50fc2
Johan Larsson <johan.larsson1989@gmail.com>
2018-04-13 20:45:02 +0100

    added json files

---
+++ app/src/main/java/glucosense/org/JSONIngredient.kt
@@ -0,0 +1,7 @@
+package glucosense.org
+
+data class JSONIngredient(
+        val id: String,
+        val name: String? = "",
+        val quantity: String? = ""
+)
---
+++ app/src/main/java/glucosense/org/JSONMeal.kt
@@ -0,0 +1,6 @@
+package glucosense.org
+
+data class JSONMeal(
+        val id: String,
+        val ingredients: ArrayList<Ingredient>
+)
