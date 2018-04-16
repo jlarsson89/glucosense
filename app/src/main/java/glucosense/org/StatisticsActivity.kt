2ae99d3ea868488daf0b3e2bb66ce69434475994
Johan Larsson <johan.larsson1989@gmail.com>
2018-04-17 00:09:36 +0100

    added klaxon json parser

--- app/src/main/java/glucosense/org/StatisticsActivity.kt
+++ app/src/main/java/glucosense/org/StatisticsActivity.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.beust.klaxon.JsonReader
+import com.beust.klaxon.Klaxon
 import com.squareup.moshi.Moshi
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_statistics.*
 import org.jetbrains.anko.custom.async
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.StringReader
 import java.net.URL
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -20,7 +25,7 @@ class StatisticsActivity : AppCompatActivity() {
     var realm = Realm.getDefaultInstance()
     val key: String = "NO01wqhp8hVdKJMgJRQqyu6syG9lwCUyBML6tmJE"
     //var url = "https://api.nal.usda.gov/ndb/search/?format=json&q=$food&sort=n&max=25&offset=0&api_key=$key"
-
+    data class Nutrient(val name: String, val value: String)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
@@ -56,11 +61,33 @@ class StatisticsActivity : AppCompatActivity() {
 
     fun parseItem(json: String) {
         val moshi = Moshi.Builder().build()
-        val jsonAdapter = moshi.adapter<Array<Report>>(Array<Report>::class.java)
-        Log.i("jsonadapter", jsonAdapter.toString())
-        var ingredients: Array<Report>? = null
-        try {
-            ingredients = jsonAdapter.fromJson(json)
+        val klaxon = Klaxon()
+        //val jsonAdapter = moshi.adapter<Array<Report>>(Array<Report>::class.java)
+        val jsonObj = JSONObject(json)
+        JsonReader(StringReader(jsonObj.toString())).use { reader ->
+            val results = arrayListOf<Nutrient>()
+            reader.beginArray {
+                while (reader.hasNext()) {
+                    val nutrient = klaxon.parse<Nutrient>(reader)
+                    Log.i("nutrient", nutrient.toString())
+                }
+            }
+        }
+        Log.i("jsonobj", jsonObj.toString())
+        //Log.i("jsonadapter", jsonAdapter.toString())
+        val ingredients: Array<Report>? = null
+        val report: JSONArray = jsonObj.getJSONArray("report")
+        val foods: JSONArray = jsonObj.getJSONArray("foods")
+        for (i in 0 until report.length()) {
+            val item = report.getJSONObject(i)
+            Log.i("report", item.toString())
+        }
+        for (i in 0 until foods.length()) {
+            val item = foods.getJSONObject(i)
+            Log.i("foods", item.toString())
+        }
+        /*try {
+            //ingredients = jsonAdapter.fromJson(json)
         }
         catch (e: Exception) {
             e.stackTrace
@@ -71,6 +98,6 @@ class StatisticsActivity : AppCompatActivity() {
         }
         for (i in ingredients!!) {
             Log.i("i", i.toString())
-        }
+        }*/
     }
 }
